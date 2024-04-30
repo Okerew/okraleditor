a1e08b37bfc1e7908bbe88e038aec4098b81626e
Okerew <witoldwarchol0@gmail.com>
2024-04-30 22:46:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,4 +11,5 @@ def os_function():
 
 os_function()
 os.system("git clone https://github.com/ajaxorg/ace-builds.git libs/ace-builds-1.5.0")
+os.system("yarn add electron-builder --dev")
 os.system("yarn build")
