b0317e1fb2f493892a7e491aaab588dd30563384
Okerew <okerewgroup@proton.me>
2024-05-10 19:52:53 +0200

    Update run_code.js

--- web_sources/run_code.js
+++ web_sources/run_code.js
@@ -12,8 +12,7 @@ function executeJavaScriptCode() {
 
     const nodeCode = activeEditor.getValue();
 
-    // Get the system's downloads folder path
-    const downloadsPath = path.join(os.homedir(), 'Downloads');
+    const downloadsPath = path.join(os.homedir(), 'Documents', 'OCE', 'Temps');
 
     // Create a temporary file in downloads folder
     const tempFilePath = path.join(downloadsPath, 'tempCodeRunner.js');
@@ -47,7 +46,7 @@ function executePythonCode() {
     const pythonCode = activeEditor.getValue();
 
     // Get the system's downloads folder path
-    const downloadsPath = path.join(os.homedir(), 'Downloads');
+    const downloadsPath = path.join(os.homedir(), 'Documents', 'OCE', 'Temps');
 
     // Create a temporary file in downloads folder
     const tempFilePath = path.join(downloadsPath, 'tempCodeRunner.py');
@@ -143,7 +142,7 @@ function executeGoCode() {
     const goCode = activeEditor.getValue();
 
     // Get the system's downloads folder path
-    const downloadsPath = path.join(os.homedir(), 'Downloads');
+    const downloadsPath = path.join(os.homedir(), 'Documents', 'OCE', 'Temps');
 
     // Create a temporary file in downloads folder
     const tempFilePath = path.join(downloadsPath, 'tempCodeRunner.go');
@@ -176,7 +175,7 @@ function executeRubyCode() {
     const rubyCode = activeEditor.getValue();
 
     // Get the system's downloads folder path
-    const downloadsPath = path.join(os.homedir(), 'Downloads');
+    const downloadsPath = path.join(os.homedir(), 'Documents', 'OCE', 'Temps');
 
     // Create a temporary file in downloads folder
     const tempFilePath = path.join(downloadsPath, 'tempCodeRunner.rb');
@@ -208,7 +207,7 @@ function executeRustCode() {
     const rustCode = activeEditor.getValue();
 
     // Get the system's downloads folder path
-    const downloadsPath = path.join(os.homedir(), 'Downloads');
+    const downloadsPath = path.join(os.homedir(), 'Documents', 'OCE', 'Temps');
 
     // Create a temporary directory in downloads folder
     const tempDirPath = path.join(downloadsPath, 'tempCodeRunnerRust');
@@ -297,4 +296,324 @@ function generateCargoToml(dependencies) {
 
 function deleteTempDir(tempDirPath) {
     fs.rmdirSync(tempDirPath, { recursive: true });
+}
+
+function executeKotlinCode() {
+    const activeTab = document.querySelector(".tab.active");
+    if (!activeTab) return;
+
+    const editorId = activeTab.getAttribute("data-editor-id");
+    const activeEditor = ace.edit(editorId);
+    if (!activeEditor) return;
+
+    const kotlinCode = activeEditor.getValue();
+
+    // Get the system's downloads folder path
+    const downloadsPath = path.join(os.homedir(), 'Documents', 'OCE', 'Temps');
+
+    // Create a temporary file in downloads folder
+    const tempFilePath = path.join(downloadsPath, 'tempCodeRunner.kt');
+    fs.writeFileSync(tempFilePath, kotlinCode);
+
+    exec(`kotlin ${tempFilePath}`, (error, stdout, stderr) => {
+        fs.unlinkSync(tempFilePath);
+
+        if (error) {
+            console.error('Error executing Kotlin code:', error.message);
+            return;
+        }
+        if (stderr) {
+            console.error('Kotlin program encountered an error:', stderr);
+            return;
+        }
+        console.log(stdout);
+    });
+}
+
+function executeCppCode() {
+    const activeTab = document.querySelector(".tab.active");
+    if (!activeTab) return;
+
+    const editorId = activeTab.getAttribute("data-editor-id");
+    const activeEditor = ace.edit(editorId);
+    if (!activeEditor) return;
+
+    const cppCode = activeEditor.getValue();
+
+    // Get the system's downloads folder path
+    const downloadsPath = path.join(os.homedir(), 'Documents', 'OCE', 'Temps');
+
+    // Create a temporary directory in downloads folder
+    const tempDirPath = path.join(downloadsPath, 'tempCodeRunnerCpp');
+    fs.mkdirSync(tempDirPath);
+
+    // Create a source file in the directory
+    const tempFilePath = path.join(tempDirPath, 'main.cpp');
+    fs.writeFileSync(tempFilePath, cppCode);
+
+    // Generate CMakeLists.txt
+    const cmakeContent = generateCMakeListsForCpp(tempDirPath, cppCode);
+    const cmakeFilePath = path.join(tempDirPath, 'CMakeLists.txt');
+    fs.writeFileSync(cmakeFilePath, cmakeContent);
+
+    // Compile and execute using CMake
+    exec(`cd ${tempDirPath} && cmake . && make && ./main`, (error, stdout, stderr) => {
+        deleteTempDir(tempDirPath);
+
+        if (error) {
+            console.error('Error executing C++ code:', error.message);
+        }
+        if (stderr) {
+            console.error('C++ program encountered an error:', stderr);
+            return;
+        }
+        console.log(stdout);
+    });
+}
+
+function generateCMakeListsForCpp(tempDirPath, cppCode) {
+    let cmakeContent = `cmake_minimum_required(VERSION 3.10)\n`;
+    cmakeContent += `project(tempCodeRunnerCpp)\n\n`;
+
+    cmakeContent += `add_executable(main main.cpp)\n`;
+
+    // List of standard libraries to skip
+    const stdCLibraries = [
+        "cstddef",
+        "cstdlib",
+        "version",
+        "limits",
+        "climits",
+        "cfloat",
+        "cstdint",
+        "stdfloat",
+        "new",
+        "typeinfo",
+        "source_location",
+        "exception",
+        "initializer_list",
+        "compare",
+        "coroutine",
+        "csignal",
+        "csetjmp",
+        "cstdarg",
+        "concepts",
+        "stdexcept",
+        "stacktrace",
+        "cassert",
+        "cerrno",
+        "system_error",
+        "memory",
+        "memory_resource",
+        "scoped_allocator",
+        "type_traits",
+        "ratio",
+        "utility",
+        "tuple",
+        "optional",
+        "variant",
+        "any",
+        "debugging",
+        "expected",
+        "bitset",
+        "functional",
+        "typeindex",
+        "execution",
+        "charconv",
+        "format",
+        "bit",
+        "string_view",
+        "string",
+        "cctype",
+        "cwctype",
+        "cstring",
+        "cwchar",
+        "cuchar",
+        "array",
+        "deque",
+        "forward_list",
+        "list",
+        "vector",
+        "map",
+        "set",
+        "unordered_map",
+        "unordered_set",
+        "queue",
+        "stack",
+        "flat_map",
+        "flat_set",
+        "span",
+        "mdspan",
+        "iterator",
+        "ranges",
+        "generator",
+        "algorithm",
+        "numeric",
+        "cfenv",
+        "complex",
+        "random",
+        "valarray",
+        "cmath",
+        "linalg",
+        "numbers",
+        "chrono",
+        "ctime",
+        "locale",
+        "clocale",
+        "codecvt",
+        "text_encoding",
+        "iosfwd",
+        "iostream",
+        "ios",
+        "streambuf",
+        "istream",
+        "ostream",
+        "iomanip",
+        "print",
+        "sstream",
+        "spanstream",
+        "fstream",
+        "syncstream",
+        "filesystem",
+        "cstdio",
+        "cinttypes",
+        "strstream",
+        "regex",
+        "stop_token",
+        "thread",
+        "atomic",
+        "rcu",
+        "stdatomic.h",
+        "mutex",
+        "shared_mutex",
+        "condition_variable",
+        "semaphore",
+        "latch",
+        "barrier",
+        "future",
+        "hazard_pointer",
+        "cstdbool",
+        "ccomplex",
+        "ctgmath",
+        "cstdalign",
+        "ciso646"
+    ];
+
+    // Extract included libraries
+    const includedLibraries = parseIncludedLibraries(cppCode);
+    includedLibraries.forEach(lib => {
+        if (!stdCLibraries.includes(lib)) { // Check if it's not a library to skip
+            cmakeContent += `find_package(${lib})\n`;
+            cmakeContent += `target_link_libraries(main ${lib}::${lib})\n`;
+        }
+    });
+
+    return cmakeContent;
+}
+
+
+function parseIncludedLibraries(cppCode) {
+    const libraries = new Set();
+    const lines = cppCode.split('\n');
+
+    lines.forEach(line => {
+        // Check if the line contains an include directive
+        const match = line.match(/^\s*#include\s+<(.+)>/);
+        if (match) {
+            const library = match[1].split('/')[0]; // Extract the library name
+            libraries.add(library);
+        }
+    });
+
+    return Array.from(libraries);
+}
+
+function executeCCode() {
+    const activeTab = document.querySelector(".tab.active");
+    if (!activeTab) return;
+
+    const editorId = activeTab.getAttribute("data-editor-id");
+    const activeEditor = ace.edit(editorId);
+    if (!activeEditor) return;
+
+    const cCode = activeEditor.getValue();
+
+    // Get the system's downloads folder path
+    const downloadsPath = path.join(os.homedir(), 'Documents', 'OCE', 'Temps');
+
+    // Create a temporary directory in downloads folder
+    const tempDirPath = path.join(downloadsPath, 'tempCodeRunnerC');
+    fs.mkdirSync(tempDirPath);
+
+    // Create a source file in the directory
+    const tempFilePath = path.join(tempDirPath, 'main.c');
+    fs.writeFileSync(tempFilePath, cCode);
+
+    // Generate CMakeLists.txt
+    const cmakeContent = generateCMakeListsForC(tempDirPath, cCode);
+    const cmakeFilePath = path.join(tempDirPath, 'CMakeLists.txt');
+    fs.writeFileSync(cmakeFilePath, cmakeContent);
+
+    // Compile and execute using CMake
+    exec(`cd ${tempDirPath} && cmake . && make && ./main`, (error, stdout, stderr) => {
+        deleteTempDir(tempDirPath);
+
+        if (error) {
+            console.error('Error executing C code:', error.message);
+        }
+        if (stderr) {
+            console.error('C program encountered an error:', stderr);
+            return;
+        }
+        console.log(stdout);
+    });
+}
+
+function generateCMakeListsForC(tempDirPath, cCode) {
+    let cmakeContent = `cmake_minimum_required(VERSION 3.10)\n`;
+    cmakeContent += `project(tempCodeRunnerC)\n\n`;
+
+    cmakeContent += `add_executable(main main.c)\n`;
+
+    // List of C default libraries
+    const cLibraries = [
+        "assert.h",
+        "complex.h",
+        "ctype.h",
+        "errno.h",
+        "fenv.h",
+        "float.h",
+        "inttypes.h",
+        "iso646.h",
+        "limits.h",
+        "locale.h",
+        "math.h",
+        "setjmp.h",
+        "signal.h",
+        "stdalign.h",
+        "stdarg.h",
+        "stdatomic.h",
+        "stdbit.h",
+        "stdbool.h",
+        "stdckdint.h",
+        "stddef.h",
+        "stdint.h",
+        "stdio.h",
+        "stdlib.h",
+        "stdnoreturn.h",
+        "string.h",
+        "tgmath.h",
+        "threads.h",
+        "time.h",
+        "uchar.h",
+        "wchar.h",
+        "wctype.h"
+    ];
+
+    cLibraries.forEach(lib => {
+        cmakeContent += `find_package(${lib})\n`;
+        cmakeContent += `target_link_libraries(main ${lib}::${lib})\n`;
+    });
+
+    return cmakeContent;
 }
