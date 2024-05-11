d40517d1d45e3ca947b5ed18188a07982a971317
Okerew <okerewgroup@proton.me>
2024-05-11 14:02:26 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,9 @@
-
 // this is an example preload script that links to the config after the DOM is loaded
+const os = require('os');
+const path = require('path');
+
 window.addEventListener('DOMContentLoaded', () => {
     const script = document.createElement('script');
-    script.src = 'config.js';
+    script.src = path.join(os.homedir(), 'Documents', 'OCE', 'Config', 'config.js');
     document.head.appendChild(script);
 });
